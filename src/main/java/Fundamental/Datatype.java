d06cb2d7a2a9a2596176a04df55ea66c540dd7a9
Annamalai1995 <s.annamalai1995@gmail.com>
2024-10-16 15:29:18 +0530

    Datatypes

---
+++ src/main/java/Fundamental/Datatype.java
@@ -0,0 +1,17 @@
+package Fundamental;
+
+public class Datatype {
+    public static void main(String[] args) {
+        int briyani = 450;
+        float raitha = 15.5f;
+        String name="Sameer";
+        char initial='A';
+        double amount=4500.00;
+        System.out.println("Integer value"+briyani);
+        System.out.println("Float value:"+raitha);
+        System.out.println("String value:"+name);
+        System.out.println("Character value:"+initial);
+        System.out.println("Double value:"+amount);
+        System.out.println("Double value:"+amount);
+    }
+}
