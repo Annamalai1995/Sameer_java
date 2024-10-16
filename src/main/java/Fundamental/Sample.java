e83fd02ab7fe39ce0ae60c4c0643eda60bf9e049
Annamalai1995 <s.annamalai1995@gmail.com>
2024-10-16 14:46:30 +0530

    hello

---
+++ src/main/java/Fundamental/Sample.java
@@ -0,0 +1,13 @@
+package Fundamental;
+
+public class Sample
+{
+    public static void main(String[] args) {
+        int value=52;
+        int value1=150;
+        int result=value+value1;
+        System.out.println("result");
+        System.out.println(result);
+        System.out.println("The Output is:"+result);
+    }
+}
---
+++ src/main/java/Fundamental/Variables.java
@@ -0,0 +1,8 @@
+package Fundamental;
+
+public class Variables {
+    public static void main(String[] args) {
+        int sameer=10;
+
+    }
+}
