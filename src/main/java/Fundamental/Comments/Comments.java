8e355eaa536280ba3a80c63bdfbc706e80637255
Annamalai1995 <s.annamalai1995@gmail.com>
2024-11-07 15:31:54 +0530

    comments

---
+++ src/main/java/Fundamental/Comments/Comments.java
@@ -0,0 +1,20 @@
+package Fundamental.Comments;
+
+public class Comments {
+    public static void main(String[] args) {
+        //It a non executable statements
+
+
+        //Two types:
+        //single line comment - to define //
+        //multiline comment-to define  /*    */
+       /* int a=150;
+        int b=200;
+        int c=a+b;
+        System.out.println("sum  of numbers is :"+c);  */
+
+        //System.out.println("sameer");
+        System.out.println("kpr college");
+    }
+}
+
